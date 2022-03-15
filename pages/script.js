b30707bbb83f10a09162e02c79c3a057d4efc7a4
Jacob-Westley <56544642+Jacob-Westley@users.noreply.github.com>
2022-03-15 06:10:03 +0000

    Working Breathing Exercises
    Addition of Working Breathing Exercises and general progress to other sections.
    
    15/03/2022.

--- pages/script.js
+++ pages/script.js
@@ -1,20 +1,14 @@
 document.getElementById("startButton").addEventListener("click", startFunction);
-
-    function startFunction() { location.href = "http://localhost:9000/home"; }
-
 document.getElementById("aboutButton").addEventListener("click", aboutFunction);
-
-    function aboutFunction() { location.href = "http://localhost:9000/about"; }
-
 document.getElementById("breathingButton").addEventListener("click", breathingFunction);
-
-    function breathingFunction() { location.href = "http://localhost:9000/breathing"; }
-
 document.getElementById("nhsButton").addEventListener("click", nhsFunction);
-
-    function nhsFunction() { location.href = "https://www.nhs.uk/mental-health/"; }
-
 document.getElementById("mindButton").addEventListener("click", mindFunction);
+document.getElementById("backButton").addEventListener("click", backFunction);
 
+    function startFunction() { location.href = "http://localhost:9000/home"; }        
+    function aboutFunction() { location.href = "http://localhost:9000/about"; }
+    function breathingFunction() { location.href = "http://localhost:9000/breathing"; }
+    function nhsFunction() { location.href = "https://www.nhs.uk/mental-health/"; }
     function mindFunction() { location.href = "https://www.nhs.uk/mental-health/"; }
+    function backFunction() { location.href = "http://localhost:9000"; }  
 
